c978c460b36138583999c75ab4cfbb02f0d5bc79
hemanth-poondla <poondlahemanth1@gmail.com>
2025-05-24 23:20:19 +0530

    updated CORS error handling

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const generateRoute = require('./routes/generate');
+const allowedOrigins = ['http://localhost:3000', 'https://trip-wizard1.vercel.app'];
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/generate', generateRoute);
