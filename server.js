2928a78e3dcf5039392b6334c96e923a3898e0bb
hemanth-poondla <poondlahemanth1@gmail.com>
2025-06-04 16:33:18 +0530

    phase5 - server.js udpate

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); 
 const express = require('express');
 const cors = require('cors');
 const generateRoute = require('./routes/generate');
@@ -12,7 +13,7 @@ app.use(express.json());
 
 app.use('/api/generate', generateRoute);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
